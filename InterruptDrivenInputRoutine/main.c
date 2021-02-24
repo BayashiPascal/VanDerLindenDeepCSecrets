29119725bab72654c5fbdf859985eda9f7178fb3
BayashiPascal <bayashipascal@gmail.com>
2021-02-24 21:18:10 +0900

    Add InterruptDrivenInputRoutine, give up

--- InterruptDrivenInputRoutine/main.c
+++ InterruptDrivenInputRoutine/main.c
@@ -17,16 +17,6 @@ void SigPollHandler(
   siginfo_t *si,
   void *arg);
 
-void handle_signal(int a) { // Can only use async-signal safe functions here.
-    char msg[] = "got char c\n";
-    char* c = msg + (sizeof msg - 3);
-    if(1 != read(STDIN_FILENO, c, 1) || !isprint(*c))
-        return;
-    write(STDOUT_FILENO, msg, sizeof msg - 1);
-    if(*c == 'q')
-        exit(EXIT_SUCCESS);
-}
-
 void SetSigPollHandler(void) {
 
   struct sigaction sigActionSegv;
@@ -49,7 +39,7 @@ void SigPollHandler(
   int signal,
   siginfo_t *si,
   void *arg) {
-printf("sigpoll\n");fflush(stdout);
+
   char c = ' ';
   read(STDIN_FILENO, &c, 1);
   if (c == 'q') {
@@ -63,15 +53,6 @@ printf("sigpoll\n");fflush(stdout);
 }
 
 int main() {
-
-  /*SetSigPollHandler();
-  int fdStdin = open("/dev/stdin", O_ASYNC);
-  if (ioctl(fdStdin, I_SETSIG, S_RDNORM) < 0) {
-
-    printf("ioctl failed: %s\n", strerror(errno));
-    exit(EXIT_FAILURE);
-
-  }*/
 
   printf("tty %s\n",ttyname(fileno(stdin)));
   int fdStdin = open(ttyname(STDIN_FILENO), O_ASYNC);
@@ -95,3 +76,39 @@ int main() {
   return EXIT_SUCCESS;
 
 }
+
+// According to
+// https://stackoverflow.com/questions/45373879/why-i-cant-receive-sigpoll-signal-from-ioctl-function
+// The following should work, but it doesn't
+
+//#include <stdio.h>
+//#include <signal.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+//#include <fcntl.h>
+//#include <ctype.h>
+
+//void handle_signal(int a) { // Can only use async-signal safe functions here.
+    //char msg[] = "got char c\n";
+    //char* c = msg + (sizeof msg - 3);
+    //if(1 != read(STDIN_FILENO, c, 1) || !isprint(*c))
+        //return;
+    //write(STDOUT_FILENO, msg, sizeof msg - 1);
+    //if(*c == 'q')
+        //exit(EXIT_SUCCESS);
+//}
+
+//int main() {
+    //printf("type q to exit\n");
+
+    //signal(SIGIO, handle_signal);
+    //fcntl(STDIN_FILENO, F_SETFL, O_ASYNC | fcntl(STDIN_FILENO, F_GETFL));
+    //fcntl(STDIN_FILENO, F_SETOWN, getpid());
+
+    //sigset_t mask;
+    //sigemptyset(&mask);
+    //for(;;)
+        //sigsuspend(&mask);
+
+    //return EXIT_SUCCESS;
+//}
